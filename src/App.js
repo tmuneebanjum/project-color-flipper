53e51eb10862c606178a3f7ec2c548f79ab449c1
Muneeb Anjum <tmuneebanjum@gmail.com>
2021-07-14 09:11:49 -0700

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 function App() {
+  const [colorCode, setColorCode] = useState("#f8fafc");
+
+  function getRandom() {
+    return Math.floor(Math.random() * hex.length);
+  }
+
+  function changeBg() {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[getRandom()];
+    }
+    setColorCode(hexColor);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: colorCode }}>
+      <div className="nav-center">
+        <h2 className="text-center text-3xl shadow-2xl p-4 font-mono text-yellow-400">
+          Colour Flipper
+        </h2>
+      </div>
+
+      <div className="bg-transparent flex items-center justify-center min-h-screen text-center">
+        <div>
+          <h1 className="bg-black text-white p-8 rounded px-16 text-3xl  font-mono">
+            Background Color:
+            <span
+              className="color
+    text-purple-600"
+            >
+              {colorCode}
+            </span>
+          </h1>
+          <button
+            className="mt-3 p-4 px-6 rounded bg-transparent border-4 hover:bg-black hover:text-white hover:border-black text-2xl py-3"
+            id="btn"
+            onClick={changeBg}
+          >
+            click me
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
